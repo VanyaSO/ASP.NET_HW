5dabc208da4adb7bc60c7591c5ea6fbe226ea808
Ivan Ushachov <ushachovg324@gmail.com>
2025-01-12 13:58:47 +0200

    add hw_15

---
+++ hw_15/hw_15/ViewModels/EmailLetterViewModel.cs
@@ -0,0 +1,9 @@
+namespace hw_15.ViewModels;
+
+public class EmailLetterViewModel
+{
+    public string Email { get; set; }
+    public string Title { get; set; }
+    public string Comment { get; set; }
+    public DateOnly Date { get; set; }
+}
