d0d35c4d6842d8b559d34a04ab72e7a3eb05c4fa
Ivan Ushachov <90464856+VanyaSO@users.noreply.github.com>
2025-01-12 14:12:13 +0200

    Delete hw_15 directory

--- hw_15/hw_15/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace hw_15.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- hw_15/hw_15/ViewModels/EmailLetterViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace hw_15.ViewModels;
-
-public class EmailLetterViewModel
-{
-    public string Email { get; set; }
-    public string Title { get; set; }
-    public string Comment { get; set; }
-    public DateOnly Date { get; set; }
-}
